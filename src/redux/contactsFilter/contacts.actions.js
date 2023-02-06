7edc2d2af1746d6760e88be4da779b530daa936d
Natalya Karpova <knataly0513@gmail.com>
2023-02-06 15:00:59 +0200

    Delete contacts.actions.js

--- src/redux/contactsFilter/contacts.actions.js
+++
@@ -1,5 +0,0 @@
-import { FILTER, SET_CONTACTS, DELETE_CONTACTS } from "./contacts.types";
-
-export const filterInputAction = payload => ({ type: FILTER, payload });
-export const setContactsAction = payload => ({ type: SET_CONTACTS, payload });
-export const deleteContactsAction = payload => ({ type: DELETE_CONTACTS, payload });
